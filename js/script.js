8f13f3ca0ee74b9640d6113b34eeb00a2a0d8e08
fadqurrosyidik <fadqurrosyidik@privy.tech>
2022-12-31 09:58:44 +0700

    fix style location

--- js/script.js
+++ js/script.js
@@ -792,4 +792,12 @@
 
     invitationName.textContent = name;
 
+    function copyToClipboard(element) {
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val($(element).text()).select();
+        document.execCommand("copy");
+        $temp.remove();
+    }
+
 })(window.jQuery);
